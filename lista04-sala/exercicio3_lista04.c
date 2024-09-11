bf331ab7768ee9c7d1b66a68f8d32ae4bffb094e
Victor Monteiro <victor.hms1206@gmail.com>
2024-09-11 16:03:00 -0300

    mudanças de arquivo e inicio do ex3

---
+++ lista04-sala/exercicio3_lista04.c
@@ -0,0 +1,22 @@
+//3. Faça um programa que:
+// e Peça para o usuário entrar com o nome e a posição (coordenadas X e Y) de N cidades e as armazene em um vetor de estruturas (N e informado pelo usuário);
+// e Crie uma matriz de distâncias entre cidades de tamanho N x N;
+// e Calculea distância entre cada duas cidades e armazene na matriz;
+// e Exiba na tela a matriz de distancias obtida;
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+    typedef struct cidade
+    {
+        char nome_cidade[50];
+        float coord_x;
+        float coord_y;
+    };
+    float calcularDistancia(){
+
+    }
+    int main(){
+        int N_cidades;
+
+    }
+    
--- testandoPontedeponte.c
+++ testandoPontedeponte.c
@@ -1,8 +1,3 @@
-//3. Faça um programa que:
-// e Peça para o usuário entrar com o nome e a posição (coordenadas X e Y) de N cidades e as armazene em um vetor de estruturas (N e informado pelo usuário);
-// e Crie uma matriz de distâncias entre cidades de tamanho N x N;
-// e Calculea distância entre cada duas cidades e armazene na matriz;
-// e Exiba na tela a matriz de distancias obtida;
 #include<stdio.h>
 #include<stdlib.h>
     int main(){
